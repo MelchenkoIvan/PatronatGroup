c50b31774791320b437aa788d3b281731abfb7bc
imelchenko61@gmail.com <imelchenko61@gmail.com>
2021-11-21 19:55:57 +0100

    initial commit

---
+++ PatronatGroup/ClientApp/src/presentation/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Route } from 'react-router';
+import Layout from './components/Layout';
+
+import '../custom.css'
+
+export default () => (
+    <Layout>
+        {/* <Route exact path='/' component={Home} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetch-data/:startDateIndex?' component={FetchData} /> */}
+    </Layout>
+);
